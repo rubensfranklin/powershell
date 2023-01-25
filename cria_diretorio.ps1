ddb499197393797480535ddf88c701f14ad1caa1
Alan Queiroz <alanqueiroz@outlook.com>
2023-01-25 05:45:48 -0300

    Atualização script de criação de diretórios.

--- cria_diretorio.ps1
+++ cria_diretorio.ps1
@@ -1,15 +1,15 @@
-$dirName = Get-Content C:\Users\alanq\Documents\sigla.txt | Select-Object -Index 1
+$dirName = Get-Content "C:\Users\alanq\Documents\sigla.txt" | Select-Object -index 0
 $caminho = "C:\Users\alanq\Documents"
-$checkLine = Get-Content $caminho\$dirName | Select-Object -Index 1
 $timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 if (Test-Path $caminho\$dirName) {
-    Write-Host "O diretório $caminho\$dirName já existe."
-    Add-Content -Path "C:\LOGS\log.txt" -Value "[$timeStamp] O diretório $dirName já existe."
+    Write-Host "O diretório $dirName já existe."
+    Add-Content -Path "C:\LOGS\log.txt" -Value "[$timeStamp] O diretório $caminho\$dirName já existe."
 }
 else {
     New-Item -ItemType Directory -Path $caminho\$dirName
-    Add-Content -Path "C:\LOGS\log.txt" -Value "[$timeStamp] O diretório $dirName foi criado em $dirName"
-    if ($checkLine -eq "criar") {
+    Add-Content -Path "C:\LOGS\log.txt" -Value "[$timeStamp] O diretório $dirName foi criado em $caminho\$dirName"
+    $arquivo = Get-Content "C:\Users\alanq\Documents\sigla.txt" | Select-Object -index 1
+    if ($arquivo -eq "criar") {
         New-Item -ItemType File -Path $caminho\$dirName\$dirName.txt
-    }
+    }    
 }
