f650bf47c5625efcacdc45d1ea74c95c2a7b2059
Alan Queiroz <alanqueiroz@outlook.com>
2023-02-04 08:31:17 -0300

    comparar o conteúdo de dois diretórios.

---
+++ compara-diretorios.ps1
@@ -0,0 +1,14 @@
+﻿$saidaPath = "C:\Users\alanq\Documents\temp"
+$backupPath = "C:\Users\alanq\Documents\backup"
+$timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$logs = "C:\LOGS\log.txt"
+
+Get-ChildItem $saidaPath | ForEach-Object {
+    $file = $_.Name
+    if (Test-Path "$backupPath\$file") {
+        $arquivos = Get-ChildItem $saidaPath        
+        Remove-Item "$saidaPath\$file"       
+    }
+    Add-Content -Path $logs -Value "[$timeStamp] O arquivo $arquivos foi removido do diretório $saidaPath" 
+    Write-Host "O arquivo $arquivos foi removido do diretório $saidaPath"
+}
