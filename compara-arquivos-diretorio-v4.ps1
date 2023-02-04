b58c89919713daad8c5a6b4c2f66b354d9f7f63c
Alan Queiroz <alanqueiroz@outlook.com>
2023-02-04 10:09:07 -0300

    ajuste no script

--- compara-arquivos-diretorio-v4.ps1
+++ compara-arquivos-diretorio-v4.ps1
@@ -1,4 +1,4 @@
-$saidaPath = "C:\Users\alanq\Documents\saida"
+﻿$saidaPath = "C:\Users\alanq\Documents\saida"
 $backupPath = "C:\Users\alanq\Documents\backup"
 $tempPath = "C:\temp"
 $timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
@@ -10,28 +10,34 @@ $backupFiles = Get-ChildItem -Path $backupPath
 
 $movedFiles = @()
 
-foreach ($saidaFile in $saidaFiles) {
+foreach ($saidaFile in $saidaFiles)
+{
     $backupFile = $backupFiles | Where-Object { $_.Name.Split(".")[0] -eq $saidaFile.Name.Split(".")[0] }
-    if ($backupFile) {
+    if ($backupFile)
+    {
         Move-Item -Path $saidaFile.FullName -Destination $tempPath
         $movedFiles += $saidaFile.Name
         Add-Content -Path $logs -Value "[$timeStamp] - O arquivo $saidaFile foi movido do diretório $saidaPath para o diretório temporário $tempPath"
     }
 }
 
-if ($movedFiles.Count -gt 0) {
+if ($movedFiles.Count -gt 0)
+{
     Write-Host "[$timeStamp] - Os seguintes arquivos foram movidos da pasta de SAIDA para o diretório temporário:"
     $movedFiles | ForEach-Object { Write-Host $_ }
 }
-else {
+else
+{
     Write-Host "[$timeStamp] - Não houve arquivos movidos da pasta de SAIDA."
 }
 
 # Verifica se existem arquivos com mais de 2 dias no diretório temp, serão removidos
-foreach ($tempFile in $tempFiles) {   
+foreach ($tempFile in $tempFiles)
+{   
     $fileAge = (Get-Date) - $tempFile.CreationTime    #Obtém a data de criação
     #$fileAge = (Get-Date) - $tempFile.LastWriteTime  #Obtém a data da última modificação
-    if ($fileAge.Days -gt 2) {
+    if ($fileAge.Days -gt 2)
+    {
         Remove-Item -Path $tempFile.FullName
         Add-Content -Path $logs -Value "[$timeStamp] - O arquivo $tempFile foi removido do diretório temporário $tempPath porque tem mais de 2 dias"
     }
