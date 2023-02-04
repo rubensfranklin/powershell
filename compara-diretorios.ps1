957507cac1bed37a5ada136a6ef14059bb866916
Alan Queiroz <alanqueiroz@outlook.com>
2023-02-04 08:54:27 -0300

    tratamento de log

--- compara-diretorios.ps1
+++ compara-diretorios.ps1
@@ -1,14 +1,22 @@
-﻿$saidaPath = "C:\Users\alanq\Documents\temp"
+﻿$saidaPath = "C:\Users\alanq\Documents\saida"
 $backupPath = "C:\Users\alanq\Documents\backup"
 $timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 $logs = "C:\LOGS\log.txt"
+$controle = 0
 
 Get-ChildItem $saidaPath | ForEach-Object {
     $file = $_.Name
     if (Test-Path "$backupPath\$file") {
-        $arquivos = Get-ChildItem $saidaPath        
-        Remove-Item "$saidaPath\$file"       
+        $arquivos = Get-ChildItem $saidaPath
+        Remove-Item "$saidaPath\$file"   
+        $controle = 1
     }
-    Add-Content -Path $logs -Value "[$timeStamp] O arquivo $arquivos foi removido do diretório $saidaPath" 
-    Write-Host "O arquivo $arquivos foi removido do diretório $saidaPath"
+ 
+    if ($controle = 1) {
+        Add-Content -Path $logs -Value "[$timeStamp] O arquivo $arquivos foram removido do diretório $saidaPath" 
+        Write-Host "O arquivo $arquivos foi removido do diretório $saidaPath"
+    }
 }
+
+
+ 
